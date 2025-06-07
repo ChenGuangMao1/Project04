96ec36cb4e5bb29f45e0caa17957bb1876d5814c
Chenguangmao <2068319065@qq.com>
2025-06-07 14:14:35 +0800

    six

--- src/MysQLjava/javaMysql.java
+++ src/MysQLjava/javaMysql.java
@@ -14,6 +14,7 @@ public class javaMysql {
         System.out.println("master222");
         System.out.println("master333");
         System.out.println("好了");
+        System.out.println("好了");
         System.out.println("hot-fix111");
         int name = 20;
         Properties properties = new Properties();
